47b06e6752939ce5a513e478d076711c493f6d49
Waleriano Metzker <99998673+MetzkerDeveloper@users.noreply.github.com>
2023-03-07 13:57:50 -0300

    First upload of my Portfolio

---
+++ porfolio/script.js
@@ -0,0 +1,8 @@
+
+var Typed = new Typed(".multiple-text", {
+    strings:['Developer Front-end', 'Developer No/Low-Code', 'Developer Full Stack'],
+    typedSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
