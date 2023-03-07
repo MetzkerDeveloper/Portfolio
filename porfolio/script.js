af0b6d99348d7f6fc031c4e14efe6989d3c838dd
Waleriano Metzker <99998673+MetzkerDeveloper@users.noreply.github.com>
2023-03-07 14:05:48 -0300

    Delete porfolio directory

--- porfolio/script.js
+++
@@ -1,8 +0,0 @@
-
-var Typed = new Typed(".multiple-text", {
-    strings:['Developer Front-end', 'Developer No/Low-Code', 'Developer Full Stack'],
-    typedSpeed: 100,
-    backSpeed: 50,
-    backDelay: 1000,
-    loop: true
-})
